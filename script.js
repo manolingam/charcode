5111d5e3b7e8d4ad2ba50cd800b16e6ea495579c
Manolingam <manolingam1996@gmail.com>
2019-01-30 22:04:37 +0530

    minor update

--- script.js
+++ script.js
@@ -6,10 +6,17 @@ addEventListener('keypress', function(event) {
         p.innerHTML = '<p>You pressed <span>' + event.key + '</span> and it\'s <span id=\'keycode\'>Key Code</span> is <span>' + event.keyCode + '</span></p>';
         var spans = document.getElementsByTagName('span')
         spans[0].style.color = 'crimsonred'
-        spans[1].style.color = 'bluevoilet'
+        spans[1].style.color = 'bluevoilet' 
     }
     else {
-        p.innerHTML = '<p>You pressed <span>' + event.key + '</span> and it\'s <span id=\'charcode\'>Char Code</span> is <span>' + event.which + '</span></p>';
+        if (event.key === ' '){
+            p.innerHTML = '<p>You pressed <span>' + 'Space' + '</span> and it\'s <span id=\'keycode\'>Key Code</span> is <span>' + event.keyCode + '</span></p>';
+            var spans = document.getElementsByTagName('span')
+            spans[0].style.color = 'crimsonred'
+            spans[1].style.color = 'bluevoilet' 
+        } else {
+            p.innerHTML = '<p>You pressed <span>' + event.key + '</span> and it\'s <span id=\'charcode\'>Char Code</span> is <span>' + event.which + '</span></p>';
+        }
     }    
 })
 
