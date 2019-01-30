a7abfe9cbcda31442d2abf5972bfa8f5f7979b84
Manolingam <manolingam1996@gmail.com>
2019-01-30 21:35:01 +0530

    fixed a smartphone bug.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 var p = document.getElementsByTagName('p')[0]
+var input = document.querySelector('input')
 
 addEventListener('keypress', function(event) {
     if (event.which === 0) {
@@ -7,12 +8,22 @@ addEventListener('keypress', function(event) {
         var spans = document.getElementsByTagName('span')
         spans[0].style.color = 'crimsonred'
         spans[1].style.color = 'bluevoilet'
-        console.log(event)
     }
     else {
         p.innerHTML = '<p>You pressed <span>' + event.key + '</span> and it\'s <span id=\'charcode\'>Char Code</span> is <span>' + event.which + '</span></p>';
-        console.log(event)
-    }
+    }    
+})
 
-    
+input.addEventListener('keyup', function(event){
+    if (event.which === 0) {
+        p.innerHTML = '<p>You pressed <span>' + event.key + '</span> and it\'s <span id=\'keycode\'>Key Code</span> is <span>' + event.keyCode + '</span></p>';
+        var spans = document.getElementsByTagName('span')
+        spans[0].style.color = 'crimsonred'
+        spans[1].style.color = 'bluevoilet'
+        input.value = ''
+    }
+    else {
+        p.innerHTML = '<p>You pressed <span>' + event.key + '</span> and it\'s <span id=\'charcode\'>Char Code</span> is <span>' + event.which + '</span></p>';
+        input.value = ''
+    }
 })
