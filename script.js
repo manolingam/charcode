ab9c373610fd5f81737d25b03175b381390107fc
Manolingam <manolingam1996@gmail.com>
2019-01-30 20:28:38 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+var p = document.getElementsByTagName('p')[0]
+
+addEventListener('keypress', function(event) {
+    if (event.which === 0) {
+        p.innerHTML = '<p>You pressed <span>' + event.key + '</span> and it\'s <span id=\'keycode\'>Key Code</span> is <span>' + event.keyCode + '</span></p>';
+        var spans = document.getElementsByTagName('span')
+        spans[0].style.color = 'crimsonred'
+        spans[1].style.color = 'bluevoilet'
+        console.log(event)
+    }
+    else {
+        p.innerHTML = '<p>You pressed <span>' + event.key + '</span> and it\'s <span id=\'charcode\'>Char Code</span> is <span>' + event.which + '</span></p>';
+        console.log(event)
+    }
+
+    
+})
