78cfa0efa3901c3dc2a16b01984f44833b152ad8
Manolingam <manolingam1996@gmail.com>
2019-01-30 21:53:29 +0530

    removed smartphone support

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
 var p = document.getElementsByTagName('p')[0]
-var input = document.querySelector('input')
 
 addEventListener('keypress', function(event) {
     if (event.which === 0) {
@@ -14,16 +13,3 @@ addEventListener('keypress', function(event) {
     }    
 })
 
-input.addEventListener('keypress', function(event){
-    if (event.which === 0) {
-        p.innerHTML = '<p>You pressed <span>' + event.key + '</span> and it\'s <span id=\'keycode\'>Key Code</span> is <span>' + event.keyCode + '</span></p>';
-        var spans = document.getElementsByTagName('span')
-        spans[0].style.color = 'crimsonred'
-        spans[1].style.color = 'bluevoilet'
-        input.value = ''
-    }
-    else {
-        p.innerHTML = '<p>You pressed <span>' + event.key + '</span> and it\'s <span id=\'charcode\'>Char Code</span> is <span>' + event.which + '</span></p>';
-        input.value = ''
-    }
-})
